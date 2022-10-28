d43dd89ba2f9affb602bd329e848159a848bfb79
Khoa <khoantgcd201759@fpt.edu.vn>
2022-10-28 12:47:43 +0700

    update template

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -31,10 +31,10 @@ class RegistrationFormType extends AbstractType
             ])
             ->add('phoneNumber')
             ->add('role', ChoiceType::class,
-            ['choices'  => [
-                'Customer' => 'ROLE_CUSTOMER',
-                'Seller' => 'ROLE_SELLER',
-            ],'mapped' => false,])
+                ['choices'  => [
+                    'Customer' => 'ROLE_CUSTOMER',
+                    'Seller' => 'ROLE_SELLER',
+                ],'mapped' => false,])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -45,7 +45,7 @@ class RegistrationFormType extends AbstractType
                         'message' => 'Please enter a password',
                     ]),
                     new Length([
-                        'min' => 6,->add('phoneNumber')
+                        'min' => 6,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
